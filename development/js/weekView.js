ac6bd0bd754b0df246ecbb422be1c7ea35db0bdc
Tomasz Wroński <tomashh11@gmail.com>
2020-07-27 15:35:34 +0200

    add functionality of next and prev buttons

--- development/js/weekView.js
+++ development/js/weekView.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
     ];
 
     const maxWeekNumber = 52;
+    const minWeekNumber = 1;
 
     const weekPlanContainer = document.querySelector(".weekPlanContainer");
     const weekNumber = weekPlanContainer.querySelector(".weekNumber");
@@ -27,11 +28,13 @@ document.addEventListener('DOMContentLoaded', function () {
         return 1 + Math.ceil((firstThursday - todayDate) / 604800000);
     };
 
-    const currentWeekNumber = getWeekNumber(new Date());
+    let currentWeekNumber = getWeekNumber(new Date());
 
     let plansFromStorage = new Map(JSON.parse(localStorage.getItem("plans")));
 
     let currentPlan = resolveCurrentWeekPlan(currentWeekNumber, plansFromStorage);
+
+    let currentPlanWeekNumber = Number(currentPlan.weekNumber);
 
     weekNumber.innerHTML = `Twój plan na ${currentPlan.weekNumber} tydzień:`;
 
@@ -67,17 +70,81 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             }
         }
-    }
+    };
 
     const prevNextContainer = document.querySelector(".prevNextContainer");
     const nextBtn = prevNextContainer.querySelector(".next");
     const prevBtn = prevNextContainer.querySelector(".prev");
 
+    function updateCurrentNextWeekNumber() {
+        for (let i = currentPlanWeekNumber + 1; i <= maxWeekNumber; i++) {
+            if (plansFromStorage.has(i.toString())) {
+                currentPlanWeekNumber = i;
+                return currentPlanWeekNumber;
+            }
+        }
+    };
+
     nextBtn.addEventListener('click', function () {
-    console.log("next klikniety")
+
+        updateCurrentNextWeekNumber();
+
+        weekNumber.innerHTML = `Twój plan na ${currentPlanWeekNumber} tydzień:`;
+
+        let planAfterCurrent = plansFromStorage.get(currentPlanWeekNumber.toString());
+
+        for (let i = 0; i <= numberOfMeals; i++) {
+            for (let weekDay of weekDays) {
+                let dayName = weekDay.dayName;
+
+                let firstBreakfast = planAfterCurrent.days[weekDay.dayNumber].firstBreakfast;
+                let secondBreakfast = planAfterCurrent.days[weekDay.dayNumber].secondBreakfast;
+                let soup = planAfterCurrent.days[weekDay.dayNumber].soup;
+                let mainCourse = planAfterCurrent.days[weekDay.dayNumber].mainCourse;
+                let supper = planAfterCurrent.days[weekDay.dayNumber].supper;
+
+                weekPlanContainer.querySelector(`.firstBreakfast${dayName}`).innerText = firstBreakfast;
+                weekPlanContainer.querySelector(`.secondBreakfast${dayName}`).innerText = secondBreakfast;
+                weekPlanContainer.querySelector(`.soup${dayName}`).innerText = soup;
+                weekPlanContainer.querySelector(`.mainCourse${dayName}`).innerText = mainCourse;
+                weekPlanContainer.querySelector(`.supper${dayName}`).innerText = supper;
+            }
+        }
     });
 
+    function updateCurrentBeforeWeekNumber() {
+        for (let i = currentPlanWeekNumber - 1; i >= minWeekNumber; i--) {
+            if (plansFromStorage.has(i.toString())) {
+                currentPlanWeekNumber = i;
+                return currentPlanWeekNumber;
+            }
+        }
+    };
+
     prevBtn.addEventListener('click', function () {
-        console.log('prev klikniety')
+
+        updateCurrentBeforeWeekNumber();
+
+        weekNumber.innerHTML = `Twój plan na ${currentPlanWeekNumber} tydzień:`;
+
+        let planBeforeCurrent = plansFromStorage.get(currentPlanWeekNumber.toString());
+
+        for (let i = 0; i <= numberOfMeals; i++) {
+            for (let weekDay of weekDays) {
+                let dayName = weekDay.dayName;
+
+                let firstBreakfast = planBeforeCurrent.days[weekDay.dayNumber].firstBreakfast;
+                let secondBreakfast = planBeforeCurrent.days[weekDay.dayNumber].secondBreakfast;
+                let soup = planBeforeCurrent.days[weekDay.dayNumber].soup;
+                let mainCourse = planBeforeCurrent.days[weekDay.dayNumber].mainCourse;
+                let supper = planBeforeCurrent.days[weekDay.dayNumber].supper;
+
+                weekPlanContainer.querySelector(`.firstBreakfast${dayName}`).innerText = firstBreakfast;
+                weekPlanContainer.querySelector(`.secondBreakfast${dayName}`).innerText = secondBreakfast;
+                weekPlanContainer.querySelector(`.soup${dayName}`).innerText = soup;
+                weekPlanContainer.querySelector(`.mainCourse${dayName}`).innerText = mainCourse;
+                weekPlanContainer.querySelector(`.supper${dayName}`).innerText = supper;
+            }
+        }
     });
 });
